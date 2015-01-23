e6d83637ff5d6f0676ca8a8d31d9fa0edcc3859b
Denis Stepulenok <super.denis@gmail.com>
2015-01-23 22:55:45 +0300

    Save changes

--- internet_on.cmd
+++ internet_on.cmd
@@ -2,6 +2,8 @@ echo Включаю DHCP чтобы работал Интернет
 netsh interface ip set address "Ethernet" dhcp
 netsh interface ip set address "Local Area Connection" dhcp
 netsh interface ip set address "Подключение по локальной сети" dhcp
+netsh interface ip set address "Wireless Network Connection" dhcp
+
 if %CLASS%==29 (
   echo Mac class - Local Network
   netsh interface ip set address name="Беспроводное сетевое соединение" dhcp

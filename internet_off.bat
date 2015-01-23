5a407dee9d120f470d3139390611aabeeb8cc63d
Denis Stepulenok <super.denis@gmail.com>
2015-01-23 22:48:44 +0300

    Save changes

--- internet_off.bat
+++ internet_off.bat
@@ -1,12 +1,18 @@
 rem @echo off
+
+set MY=10.%CLASS%.%COMP%.1
+set MASK=255.255.255.0
+set GATEWAY=10.1.1.1	
+
 if %CLASS%==30 (
   echo Windows 8 - Local Network
-  netsh interface ip set address name="Ethernet" static 192.168.%CLASS%.%COMP% 255.255.0.0 192.168.28.1 1
+  netsh interface ip set address name="Ethernet" static %MY% %MASK% %GATEWAY% 1
 ) else if %CLASS%==29 (
   echo Mac class - Local Network
-  netsh interface ip set address name="Беспроводное сетевое соединение" static 192.168.%CLASS%.%COMP% 255.255.0.0 192.168.28.1 1
+  netsh interface ip set address name="Беспроводное сетевое соединение" static %MY% %MASK% %GATEWAY% 1
 ) else (
-  netsh interface ip set address name="Local Area Connection" static 192.168.%CLASS%.%COMP% 255.255.0.0 192.168.28.1 1
-  netsh interface ip set address name="Подключение по локальной сети" static 192.168.%CLASS%.%COMP% 255.255.0.0 192.168.28.1 1
+  netsh interface ip set address name="Local Area Connection" static %MY% %MASK% %GATEWAY% 1
+  netsh interface ip set address name="Подключение по локальной сети" static %MY% %MASK% %GATEWAY% 1
+  netsh interface ip set address name="Подключение по локальной сети 2" static %MY% %MASK% %GATEWAY% 1
 )
 pause
